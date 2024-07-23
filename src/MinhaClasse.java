ea7bf1d1412192847a472c80bee32de40b4a209c
Ralf Mota <ralfdiegomota@gmail.com>
2024-07-22 21:10:02 -0400

    first commit

---
+++ src/MinhaClasse.java
@@ -0,0 +1,19 @@
+public class MinhaClasse {
+   
+    public static void main(String[] args) {
+        String primeiroNome = "Ralf";
+        String segundoNome = "Mota";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+     
+        public static String nomeCompleto (String primeiroNome, segundoNome) {
+            return "Resultado do método" + primeiroNome.concat("").concat(segundoNome);
+        }
+
+        
+
+
+    }
+    
+}
---
+++ src/Operadores.java
@@ -0,0 +1,38 @@
+public class Operadores {
+    public static void main(String[] args) {
+    
+    /*   boolean variavel = true;
+
+        System.out.println(!variavel);
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+        
+
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        String resultado = a ==b ?"verdadeiro" : "falso";
+
+        System.out.println(resultado);
+
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numeroUm == numeroDpis;
+
+        System.out.println("numeroUm é igual a numeroDois" + simNao);
+        
+*/
+        
+
+
+
+
+
+    }
+}
